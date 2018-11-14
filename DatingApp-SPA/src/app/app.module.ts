efeecdfeaaee68c6a2b5e0a5eb6556322e219831
Abhishek Kulkarni <abhishelligikar@gmail.com>
2018-11-14 17:59:38 +0530

    14-11-2018

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+
 
 @NgModule({
    declarations: [
@@ -40,7 +42,8 @@ import { AuthGuard } from './_guards/auth.guard';
       AuthService,
       ErrorInterceptorProvider,
       AlertifyService,
-      AuthGuard
+      AuthGuard,
+      UserService
    ],
    bootstrap: [
       AppComponent
