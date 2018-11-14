fd5ebc8530498a63f429dbc8f4089603c1a5ff0c
Abhishek Kulkarni <abhishelligikar@gmail.com>
2018-11-14 15:29:26 +0530

    Avoiding Seeder class to Seed Users after once it is Initiated

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -90,7 +90,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            seeder.SeedUsers();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
