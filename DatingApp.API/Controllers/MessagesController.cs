e5c12e63861deafb59fe6977a25e1f0094f8687f
Abhishelligikar <abhishelligikar@gmail.com>
2018-12-18 17:32:31 +0530

    Adding MessageController

---
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    [ServiceFilter(typeof(LogUserActivity))]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IDatingRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # DatingApp
+Angular Extensions used:
+1. C#
+2. C# Extensions
+3. Nuget Package Manager
+4. Angular Files
+5. Angular Language Services
+6. Angular v7 Snippets
+7. Angular2-switcher
+8. Auto Import
+9. Auto Rename Tag
+10. Bracket Pair Colorizer
+11. Debugger for Chrome
+12. Material icon theme
+13. Move TS
+14. Path Intellisense
+15. Prettier - Code formatter
+16. TSLint
+
+
+
