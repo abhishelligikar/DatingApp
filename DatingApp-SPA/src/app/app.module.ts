e9394ca9ea5423a06d9532a2b0aec8dab6806a0d
Abhishelligikar <abhishelligikar@gmail.com>
2018-12-19 18:19:26 +0530

    MessageComponent Added

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -27,11 +27,12 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
-import { MemberDetailResolver } from './_resolvers/member.detail-resolver';
-import { MemberListResolver } from './_resolvers/member.list-resolver';
-import { MemberEditResolver } from './_resolvers/member.edit-resolver';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { TimeAgoPipe } from 'time-ago-pipe';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -83,6 +84,7 @@ export function tokenGetter() {
       MemberListResolver,
       MemberEditResolver,
       ListsResolver,
+      MessagesResolver,
       PreventUnsavedChangesGuard
    ],
    bootstrap: [
--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,6 +39,66 @@ namespace DatingApp.API.Controllers
                 return NotFound();
 
             return Ok(messageFromRepo);
+        }
+
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var messageFromRepo = await _repo.GetMessageThread(userId, recipientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
+
+            return Ok(messageThread);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, 
+            [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageParams.userId = userId;
+            
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, 
+                messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+            
+            return Ok(messages);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+
+            if (recipient == null)
+                return BadRequest("Could not find user!");
+            
+            var message = _mapper.Map<Message>(messageForCreationDto);
+
+            _repo.Add(message);
+
+            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+
+            if (await _repo.SaveAll())
+                return CreatedAtRoute("GetMessage", new { id = message.Id}, messageToReturn);
+            
+            //throw new Exception("Creating the message failed on save.");
+            return BadRequest("Creating the message failed on save.");
+            
         }
     }
 }
